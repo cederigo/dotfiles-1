7ced6aad81016203a8ac7a841a440e3241c900a9
Cédric Reginster <cedric.reginster@gmail.com>
2016-12-29 21:21:30 +0100

    Add vim topic

---
+++ vim/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Update submodules
+git submodule foreach git pull origin master
+
+# Compile command-t c extension
+cd $ZSH/vim/bundle/command-t/ruby/command-t/
+CC=cc ruby extconf.rb
+make
+cd -
+
+# Centralized backups, swapfiles and undo history
+mkdir -p ~/.vim/{backups,swaps,undo}
+
+exit 0
